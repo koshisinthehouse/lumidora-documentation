15e1b32d1ab69035f78123d46c9ddb0520c2829f
Dominik Bruhn <dominikbruhn@googlemail.com>
2023-11-13 08:18:41 +0100

    modified

--- gui/index.js
+++ gui/index.js
@@ -1,4 +1,4 @@
-const { app, BrowserWindow, Menu } = require('electron');
+const { app, BrowserWindow, Menu, BrowserView } = require('electron');
 
 let mainWindow;
 
@@ -7,10 +7,20 @@ function createWindow() {
     mainWindow = new BrowserWindow({
         width: 800,
         height: 600,
+        webPreferences: {
+            webviewTag: true,
+            nodeIntegration: true,
+        },
     });
 
+    const view = new BrowserView()
+    mainWindow.setBrowserView(view)
+    view.setBounds({ x: 0, y: 0, width: 300, height: 300 })
+    view.webContents.loadURL('https://electronjs.org')
+
+
     // Lade eine Internetseite (z.B. google.de) im Hauptfenster
-    mainWindow.loadURL('https://www.google.de');
+    mainWindow.loadURL('https://prompthero.com/');
 
     // Erstelle ein Menü
     const template = [
@@ -29,7 +39,7 @@ function createWindow() {
     Menu.setApplicationMenu(menu);
 
     // Öffne die Entwicklertools (optional)
-    // mainWindow.webContents.openDevTools();
+    //mainWindow.webContents.openDevTools();
 
     // Reagiere auf das Schließen des Hauptfensters
     mainWindow.on('closed', function () {
@@ -46,3 +56,4 @@ app.on('window-all-closed', function () {
 app.on('activate', function () {
     if (mainWindow === null) createWindow();
 });
+
--- stable-diffusion-webui/readme.md
+++ stable-diffusion-webui/readme.md
@@ -1,27 +1,5 @@
 docker build --no-cache -t lumidora-stable-diffusion-webui .
 
-docker run --gpus all -p 7860:7860 lumidora-stable-diffusion-webui
+docker run --gpus all -p 7860:7860 -v lumidora-stable-diffusion-webui:/app/stable-diffusion-webui/outputs lumidora-stable-diffusion-webui
 
-
-
-
-docker run --gpus all -it -p 7860:7860 --entrypoint /bin/bash lumidora-stable-diffusion-webui
-
-
--v output:/output
-
-
-
-#nur mit CPU starten
-./webui.sh -f  --skip-torch-cuda-test --precision full --no-half
-
-
-#--enable-insecure-extension-access
-# gpu
-./webui.sh -f --xformers --listen
-
-#--lowvram --precision full --no-half
-
-
-
-docker run -it --name sdw --gpus all --network host -v $(pwd)/models:/app/stable-diffusion-webui/models -v $(pwd)/outputs:/app/stable-diffusion-webui/outputs --rm siutin/stable-diffusion-webui-docker:latest-cuda bash webui.sh --share
+docker exec -it [container id] /bin/bash
